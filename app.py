bc1871571da0795f6f60de1f558a70e01bace754
unknown <xian_longdi@163.com>
2025-06-27 17:05:55 +0800

    better

--- app.py
+++ app.py
@@ -106,5 +106,5 @@ def content(casenumber):
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=8000, ssl_context=('cert.pem', 'key.pem'))
     app.config['JSON_AS_ASCII'] = False
-    port = int(os.environ.get('PORT', 443))
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
